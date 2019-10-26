8e45d7aa6ecd40ff6683e9f8ef02cc6a41e1bb99
Christopher Kok <ckok@purdue.edu>
2019-10-25 23:18:27 -0500

    Testing different tools

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 The app that will attempt to stop your masturbation habit by being annoyingly intrusive
 
 ## Installation
+- Install NudeNet:
 ```
 pip install nudenet
 or
 pip install git+https://github.com/bedapudi6788/NudeNet
 ```
+- Download and extract models folder in the current directory: https://drive.google.com/open?id=1CZuylfYvVpIv9CtzqjijuWIkCgSGz0zk
 
 ## Credits
 - Huge thanks to [Bedapudi Praneeth's NSFW Classification Project](https://github.com/bedapudi6788/NudeNet), the app is largely building upon the work he's done to effectively classify nudity with an ensemble of neural networks.
--- tk-test.py
+++ tk-test.py
@@ -1,3 +1,28 @@
+# TEST 3
+from tkinter import *
+
+root = Tk()
+def retrieve_input():
+    inputValue=textBox.get("1.0","end-1c")
+    print(inputValue)
+
+textBox=Text(root, height=2, width=10)
+textBox.pack()
+buttonCommit=Button(root, height=1, width=10, text="Commit", 
+                    command=lambda: retrieve_input())
+buttonCommit.pack()
+
+w = Label(root, text="Hello, world!")
+root.overrideredirect(True)
+root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
+root.focus_set()  # <-- move focus to this widget
+root.bind("<Escape>", lambda e: e.widget.quit())
+w.pack()
+
+root.mainloop()
+
+# TEST 1
+
 # from tkinter import *
 
 # for i in range(3):
@@ -28,22 +53,32 @@
 
 #     mainloop()
 
-import tkinter as tk
-from tkinter import ttk
+# TEST 2
+
+# import tkinter as tk
+# from tkinter import ttk
 
 
-LARGE_FONT= ("Verdana", 12)
-NORM_FONT= ("Verdana", 10)
-SMALL_FONT= ("Verdana", 8)
+# LARGE_FONT= ("Verdana", 12)
+# NORM_FONT= ("Verdana", 10)
+# SMALL_FONT= ("Verdana", 8)
 
-def popupmsg(msg):
-    popup = tk.Tk()
-    popup.wm_title("!")
-    label = ttk.Label(popup, text=msg, font=NORM_FONT)
-    label.pack(side="top", fill="x", pady=10)
-    B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
-    B1.pack()
-    popup.mainloop()
+# def popupmsg(msg):
+#     popup = tk.Tk()
+#     popup.wm_title("!")
+#     label = ttk.Label(popup, text=msg, font=NORM_FONT)
+#     label.pack(side="top", fill="x", pady=10)
+#     B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
+#     B1.pack()
+#     popup.mainloop()
 
-for i in range(3):  
-    popupmsg("popup message here!")
+# # for i in range(3):  
+# #     popupmsg("popup message here!")
+
+# import tkinter as tk
+# from overlay import Window
+
+# win = Window()
+# label = tk.Label(win.root, text="Window_0")
+# label.pack()
+# Window.launch()
